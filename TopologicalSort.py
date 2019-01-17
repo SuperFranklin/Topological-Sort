b30e95b94eb124a052187412fa00ef9a003f7b0e
franek <franciszek.slupski@supermemo.com>
2019-01-17 22:28:19 +0100

    code improvement

--- TopologicalSort.py
+++ TopologicalSort.py
@@ -1,3 +1,4 @@
+import time
 graph = {0: set([1,3,6]),
          1: set([2,3]),
          2: set([3,4,5]),
@@ -8,35 +9,37 @@ graph = {0: set([1,3,6]),
 
 #start - start vertex, with outgoing degree equal to 0
 def topoSort(graph):
+    #number of in edges for every vertex in graph
     in_degree = dict()
     result = []
     queue = []
-    visited = set()
     for v in graph.keys():
         in_degree[v] = 0
     
     for key, value in graph.items():
         for v in value:
-            in_degree[v]=in_degree[v]+1        
+            in_degree[v]+=1        
     
     print(in_degree)
     
     for key, value in in_degree.items():
         if value ==0:
             queue.append(key)
-            visited.add(key)
     
     while queue:
         vertex = queue.pop()
         result.append(vertex)
         for child in graph[vertex]:
-            if child not in visited:
-                in_degree[child] = in_degree[child] -1
+            if child not in result:
+                in_degree[child]-=1
                 if in_degree[child] == 0:
                     queue.append(child)
-                    visited.add(child)
     return result
             
-    
-r = topoSort(graph)
-print(r)    
+start = time.time()
+for x in range (0,10000):
+    r = topoSort(graph)
+end = time.time()
+print(end-start)
+print(r)   
+ 
